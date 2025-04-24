f47be2c0f951685b666c11eab3ad443dd4598d6d
Daniel <cyubahirodaniel@gmail.com>
2025-04-24 16:16:01 +0200

    Add customs hook useNavigation.tsx

---
+++ hooks/useNavigation.tsx
@@ -0,0 +1,21 @@
+import {usePathname} from 'next/navigation';
+import {useMemo} from 'react';
+import {MessageSquare, Users} from 'lucide-react';
+
+export const useNavigation = () => {
+  const pathname = usePathname();
+  return useMemo(() => [
+    {
+      name: 'Conversations',
+      href: '/conversations',
+      icon: <MessageSquare/>,
+      active: pathname.startsWith('/conversations'),
+    },
+    {
+      name: 'Friends',
+      href: '/friends',
+      icon: <Users/>,
+      active: pathname.startsWith('/friends'),
+    },
+  ], [pathname]);
+};
